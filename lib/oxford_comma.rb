997dd36e11a55b8ca3553232565579d7ff0e2d4c
Amy Fogelman <abfnj1@gmail.com>
2019-06-05 10:04:03 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.length < 2
+end 	    
+  elsif array.length == 2
+    array.insert(1, "and").join(" ")
+  else
+    str = ", and " + array.pop
+    str = array.join(", ") << str
+  end
 end
